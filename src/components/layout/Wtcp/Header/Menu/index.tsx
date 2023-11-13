254ce3ec1ce0a478848527dac2c7890052bc5ac3
Eike <e-eike@weg.net>
2023-11-13 10:09:59 -0300

    correção menus

--- src/components/layout/Wtcp/Header/Menu/index.tsx
+++ src/components/layout/Wtcp/Header/Menu/index.tsx
@@ -15,27 +15,39 @@ import {
     from "@ant-design/icons";
 
 const Wtcp: React.FC = () => {
+    const boxesLogo = Array.from({ length: 4 }, (_, i) => (
+        <BoxLogo key={i} />
+    ));
+    const menuButtonsInfo = [
+        { id: 0, title: 'Nova Cotação', icon: <SnippetsOutlined /> },
+        { id: 1, title: 'Abrir Cotação', icon: <FolderOpenOutlined /> },
+        { id: 2, title: 'Salvar', icon: <SaveOutlined /> },
+        { id: 3, title: 'Imprimir', icon: <PrinterOutlined /> },
+        { id: 4, title: 'Consumir', icon: <CheckCircleOutlined /> },
+        { id: 5, title: 'Excluir', icon: <CloseCircleOutlined /> },
+        { id: 6, title: 'PCP', icon: <CalendarOutlined /> },
+        { id: 7, title: 'Configurações', icon: <SettingOutlined style={{ width: '15px' }} /> },
+        { id: 8, title: 'Informações', icon: <InfoCircleOutlined /> },
+        { id: 9, title: 'Ajuda', icon: <QuestionCircleOutlined /> },
+        { id: 10, title: 'Idioma', icon: <FlagOutlined /> },
+
+    ];
+
+    const menuButtons = menuButtonsInfo.map((button, i) => (
+        <Button className="menu-wtcp" type="primary" size="small" key={i} title={button.title}>
+            {button.icon}
+            {i === 7 && <DownOutlined style={{ fontSize: '12px' }} />}
+        </Button>
+    ));
+
     return (
         <BoxStyle>
             <HeaderWtcpMenu>
                 <WtcpMenu>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Nova Cotação"><SnippetsOutlined /></Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Abrir Cotação"><FolderOpenOutlined /></Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Salvar"><SaveOutlined /></Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Imprimir"><PrinterOutlined /></Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Consumir"><CheckCircleOutlined /></Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Excluir"><CloseCircleOutlined /></Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="PCP"><CalendarOutlined /></Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Configurações"><SettingOutlined style={{ width: '15px' }} /><DownOutlined style={{ fontSize: '12px', width: '1px' }} /> </Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Informações"><InfoCircleOutlined /></Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Ajuda"><QuestionCircleOutlined /></Button>
-                    <Button className="menu-wtcp" type="primary" size="small" title="Idioma"><FlagOutlined /></Button>
+                    <>{menuButtons}</>
                 </WtcpMenu>
                 <Logo>
-                    <BoxLogo />
-                    <BoxLogo />
-                    <BoxLogo />
-                    <BoxLogo />
+                    <>{boxesLogo}</>
                     <img src="/assets/images/WEGLogo.png" alt="WEGLogo" style={{ padding: '10px' }} />
                 </Logo>
             </HeaderWtcpMenu>
