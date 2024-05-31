ba2de0bfbcc6ae71696c84d3c87016be3d7c979c
Daniel Maidana Messerschmidt <danielmm@weg.net>
2024-05-31 14:11:39 -0300

    Deploy

--- src/app/api/services/api.ts
+++ src/app/api/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-  headers: {'Content-type':'Application/json', 'Accept':'Application/json', 'Access-Control-Allow-Origin': "*",},
-  baseURL: 'process.env.NEXT_PUBLIC_API_URL',
+  headers: {'Content-type':'Application/json', 'Accept':'Application/json'},
+  baseURL: process.env.API_URL,
   // baseURL: '//localhost:3000/',
   // baseURL: '//dcqas012168.weg.net:3255/',
 });
